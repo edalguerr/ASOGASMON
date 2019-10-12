b10b3fd0c8f08febd306068f08abcbca31229c7c
Edalguerr <eguerreroa1@gmail.com>
2019-10-11 20:28:30 -0500

    interface de usuario y servicio de inicio de sesion que se conecta al backend

---
+++ asogasmon/src/app/interfaces/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+    ID:any;
+    NOMBRE:string;
+    APELLIDOS:string;
+    EMAIL:string;
+    CONTRASENIA:string;
+    FOTO:string;
+}
---
+++ asogasmon/src/app/servicios/usuario.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  API_URL = "http://localhost/asogasmonAPI/public/api/";
+  datos:Usuario = null;
+  
+
+  constructor(private httpClient:HttpClient) { 
+    
+  }
+
+  getDatos(usuario:{email,contrasenia}) {
+   let headers = new HttpHeaders().set('Content-Type','application/json');
+   
+   return this.httpClient.post(this.API_URL+"signin",usuario,{headers: headers});
+  }
+  
+}
+
+
