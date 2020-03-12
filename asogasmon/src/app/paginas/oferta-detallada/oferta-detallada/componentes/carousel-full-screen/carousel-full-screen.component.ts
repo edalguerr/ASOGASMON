980b4d4f043cac315573abb7030e778b2f75c6d6
Edalguerr <eguerreroa1@gmail.com>
2020-03-12 11:35:00 -0500

    Oferta detallada; mostrando datos obtenidos de la API en el componente carousel-full-screen

--- asogasmon/src/app/paginas/oferta-detallada/oferta-detallada/componentes/carousel-full-screen/carousel-full-screen.component.ts
+++ asogasmon/src/app/paginas/oferta-detallada/oferta-detallada/componentes/carousel-full-screen/carousel-full-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertaDetalladaActualService } from 'src/app/servicios/ofertaDetalladaActual/oferta-detallada-actual.service';
 
 @Component({
   selector: 'app-carousel-full-screen',
@@ -7,17 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselFullScreenComponent implements OnInit {
 
-  ruta = "assets/";
-  imagenes:Array<string> = ['2015-05-07_23-04-35.jpg','Apartamento-en-Arriendo-en-Mazurn.jpg',
-  'Mini_Protoboard_170_puntos_1.jpg', 'apartamento_en_arriendo_en_cartagena_manga.jpg', 'libro-de-oro-de-matemticas-1-638.jpg',
-  '00106652174191____1__640x640.jpg', '179809-OWKTX6-319.jpg', 'B7111-BL_BATA_UPC_ml.jpg', 'bd18ca0af17b52d6278d74ec3ea0b769-product.jpg',
-  '1024o.jpg', '1024.jpg', '00106652174191____1__640x640.jpg', 'apartamento_en_arriendo_en_cartagena_manga.jpg', 'bd18ca0af17b52d6278d74ec3ea0b769-product.jpg',
-  'habitacion-arriendo_2.jpg', '2015-05-07_23-04-35.jpg'
-  ];
-
-  /*imagenes:Array<string> = ['Apartamento-en-Arriendo-en-Mazurn.jpg'];*/
+  API_URL = "http://localhost/asogasmonAPI/public/img/";
   
-  constructor() { }
+  constructor(
+    public ofertaDetalladaActualService:OfertaDetalladaActualService,
+  ) { }
 
   ngOnInit() {
   }
